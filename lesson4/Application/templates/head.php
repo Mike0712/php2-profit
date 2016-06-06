87568ba7eba09e96904cf2e346b1c12f9c1e26c3
Mike0712 <barrister2003@gmail.com>
2016-06-06 08:33:22 +0300

    FrontController + 5 задание (класс \App\SefRouter)

--- lesson4/Application/templates/head.php
+++ lesson4/Application/templates/head.php
@@ -3,6 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <title><?php echo $article['lead'] ?: 'Новостной портал';?></title>
-    <link rel="stylesheet" href="/App/templates/styles/style.css" >
+    <link rel="stylesheet" href="/Application/templates/styles/style.css" >
 </head>
 <body class="wrapper">
--- lesson4/autoload.php
+++ lesson4/autoload.php
@@ -2,8 +2,9 @@
 
 function __autoload($class)
 {
+    $vendorName = 'Application';
     $path = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
-
+    $path = str_replace('App', $vendorName, $path);
     if(is_readable($path)) {
         require $path;
     }
